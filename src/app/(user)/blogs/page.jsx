af33654d7d7d940c64d956798caa1e20b7566563
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-02-11 13:23:05 +0330

    Continue development

--- src/app/(user)/blogs/page.jsx
+++ src/app/(user)/blogs/page.jsx
@@ -4,8 +4,40 @@ import queryString from "query-string";
 import { getBlogs } from "@/services/blog/blogService";
 import BlogItems from "./components/blogs";
 import CategoryPageLayout from "../components/categories/categoriesContent/categoryPageLayout";
+import { getPageByEnglishTitle } from "@/services/pageData/pageDataServices";
 
 export const dynamic = "force-dynamic";
+
+export async function generateMetadata() {
+  try {
+    const { pageData } = await getPageByEnglishTitle("blogs");
+
+    const { metaTitle, metaDescription, tags, imageLink } = pageData;
+
+    return {
+      alternates: {
+        canonical: `/blogs`,
+      },
+      title: metaTitle,
+      description: metaDescription,
+      keywords: tags,
+      twitter: {
+        title: metaTitle,
+        description: metaDescription,
+        images: [imageLink],
+      },
+    };
+  } catch (error) {
+    console.error("Error generating metadata for product page:", error);
+    // Return a default or fallback metadata in case of an error
+    return {
+      title: "Fallback Title",
+      description: "Fallback Description",
+      keywords: [],
+      twitter: {},
+    };
+  }
+}
 
 const Blogs = async ({ searchParams }) => {
   const cookieStore = cookies();
--- src/app/(user)/products/page.jsx
+++ src/app/(user)/products/page.jsx
@@ -2,12 +2,42 @@ import { cookies } from "next/headers";
 import { toStringCookies } from "@/utils/toStringCookies";
 import queryString from "query-string";
 
-import {
-  getProducts,
-} from "@/services/product/productService";
+import { getProducts } from "@/services/product/productService";
 import ProductItems from "./components/products";
 import CategoryPageLayout from "../components/categories/categoriesContent/categoryPageLayout";
+import { getPageByEnglishTitle } from "@/services/pageData/pageDataServices";
 export const dynamic = "force-dynamic";
+
+export async function generateMetadata() {
+  try {
+    const { pageData } = await getPageByEnglishTitle("products");
+    console.log("pageData: ", pageData);
+    const { metaTitle, metaDescription, tags, imageLink } = pageData;
+
+    return {
+      alternates: {
+        canonical: `/products`,
+      },
+      title: metaTitle,
+      description: metaDescription,
+      keywords: tags,
+      twitter: {
+        title: metaTitle,
+        description: metaDescription,
+        images: [imageLink],
+      },
+    };
+  } catch (error) {
+    console.error("Error generating metadata for product page:", error);
+    // Return a default or fallback metadata in case of an error
+    return {
+      title: "Fallback Title",
+      description: "Fallback Description",
+      keywords: [],
+      twitter: {},
+    };
+  }
+}
 
 const Products = async ({ searchParams }) => {
   const cookieStore = cookies();
