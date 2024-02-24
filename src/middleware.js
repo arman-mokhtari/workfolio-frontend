e4ddaaf60a7fedd7991c85a4a9233f78a18be9a5
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-02-24 11:28:04 +0330

    debug completed

--- src/middleware.js
+++ src/middleware.js
@@ -38,55 +38,55 @@ export async function middleware(request) {
       return NextResponse.redirect(new URL("/404", url));
   }
 
-//   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
-//   const cspHeader = `
-//   default-src 'self';
-//   script-src 'self' 'nonce-${nonce}' 'strict-dynamic' 'unsafe-eval';
-//   script-src-elem 'self' 'nonce-${nonce}' https://www.googletagmanager.com;
-//   style-src 'self' 'unsafe-inline';
-//   img-src 'self' blob: data: https://cdn.workfolio.ir https://trustseal.enamad.ir https://logo.samandehi.ir https://cdn.zarinpal.com;
-//   font-src 'self';
-//   object-src 'none';
-//   base-uri 'self';
-//   form-action 'self';
-//   frame-ancestors 'none';
-//   block-all-mixed-content;
-//   upgrade-insecure-requests;
-//   connect-src 'self' https://api.workfolio.ir https://region1.google-analytics.com https://www.google-analytics.com;
-// `;
-//   // Replace newline characters and spaces
-//   const contentSecurityPolicyHeaderValue = cspHeader
-//     .replace(/\s{2,}/g, " ")
-//     .trim();
+  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+  const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'nonce-${nonce}' 'strict-dynamic' 'unsafe-eval';
+  script-src-elem 'self' 'nonce-${nonce}' https://www.googletagmanager.com;
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data: https://cdn.workfolio.ir https://trustseal.enamad.ir https://logo.samandehi.ir https://cdn.zarinpal.com;
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+  block-all-mixed-content;
+  upgrade-insecure-requests;
+  connect-src 'self' https://api.workfolio.ir https://region1.google-analytics.com https://www.google-analytics.com;
+`;
+  // Replace newline characters and spaces
+  const contentSecurityPolicyHeaderValue = cspHeader
+    .replace(/\s{2,}/g, " ")
+    .trim();
 
-//   const requestHeaders = new Headers(request.headers);
-//   requestHeaders.set("x-nonce", nonce);
-//   requestHeaders.set(
-//     "Content-Security-Policy",
-//     contentSecurityPolicyHeaderValue
-//   );
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-nonce", nonce);
+  requestHeaders.set(
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue
+  );
 
-//   const response = NextResponse.next({
-//     request: {
-//       headers: requestHeaders,
-//     },
-//   });
-//   response.headers.set(
-//     "Content-Security-Policy",
-//     contentSecurityPolicyHeaderValue
-//   );
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+  response.headers.set(
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue
+  );
 
-//   return response;
+  return response;
 }
 
-// export const config = {
-//   matcher: [
-//     {
-//       source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
-//       missing: [
-//         { type: "header", key: "next-router-prefetch" },
-//         { type: "header", key: "purpose", value: "prefetch" },
-//       ],
-//     },
-//   ],
-// };
+export const config = {
+  matcher: [
+    {
+      source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+      missing: [
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" },
+      ],
+    },
+  ],
+};
